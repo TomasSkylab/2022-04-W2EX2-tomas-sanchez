d4c2c69847c190745e402ebc482536dec1d03676
Tomás Sánchez Martínez <tomassanchezmartinez@MacBook-Pro-de-Tomas.local>
2022-04-13 13:32:43 +0200

    Added file isPalindrome.text.js and start with the test

---
+++ palindrome.test.js
@@ -0,0 +1,33 @@
+const palindrome =["Anita lava la tina","Yo hago yoga hoy", "Alli ves Sevilla", "hola me llamo ralph"];
+
+const isPalindrome (text) => {
+
+    if(typeof(text) === ){
+        throw new "text not provided"();
+
+    }
+}
+
+
+// A-A-A
+
+// Arrange: monta el escenario/ recopila valores que vas a enviar o que esperas
+
+// Act: haz que funcione la maquina
+
+// Assert: comprueba que ha pasado lo que esperas que pase
+
+Ejemplo:
+
+describe("When strictEquals function", () => {
+
+describe("When it receives (1, 1)", () => {
+
+test("Then it should return true", () => {
+
+
+});
+
+});
+
+});
